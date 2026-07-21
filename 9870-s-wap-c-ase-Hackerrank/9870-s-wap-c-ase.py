189a1bb5636f356f305e1a71cb5f576c95f665a5
gayathri-pyth <mntradersbangalore@gmail.com>
2026-07-21 09:09:35 +0530

    Update 9870-s-wap-c-ase.py

--- 9870-s-wap-c-ase-Hackerrank/9870-s-wap-c-ase.py
+++ 9870-s-wap-c-ase-Hackerrank/9870-s-wap-c-ase.py
@@ -1,8 +1,7 @@
 def swap_case(s):
     return s.swapcase()
 
-
-
-# Synced seamlessly with LeetHub Pro
-# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
